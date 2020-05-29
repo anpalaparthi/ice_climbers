707414478392fbdae02488e11a61e6e3f7d23e76
dhruvashok <47910811+dhruvashok@users.noreply.github.com>
2020-05-29 16:43:56 -0700

    added navbar

---
+++ src/components/plan.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Plan extends Component {
+	render() {
+		return(
+			<h1>Planner Page</h1>
+		)
+	}
+}
