486d18dd848ac288db348ab4996d9574d752663e
anishapv <palaparthianisha0772@students.esuhsd.org>
2020-05-30 00:20:19 -0700

    react routes with hello world setup

--- src/App.js
+++ src/App.js
@@ -3,15 +3,28 @@ import { BrowserRouter as Router, Route } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
 import HelloWorldText from "./components/HelloWorldText";
 
+import Navbar from "./components/navbar.component";
+import Home from "./components/home.component";
+import Login from "./components/login.component";
+import Plan from "./components/plan.component";
+import Timer from "./components/timer.component";
+import Track from "./components/track.component";
+
 function App() {
     return (
-      <Router>
-          <div className="container">
-            <Route path='/' exact component={HelloWorldText}/>
-              <Route path='/potato' exact component={HelloWorldText}/>
-          </div>
-      </Router>
-  );
+        <Router>
+            <Navbar />
+            <br/>
+            <Route path="/" exact component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/plan" component={Plan} />
+            <Route path="/timer" component={Timer} />
+            <Route path="/track" component={Track} />
+
+             {/*Anisha Testing out */}
+            <Route path='/potato' exact component={HelloWorldText}/>
+        </Router>
+    );
 }
 
 export default App;
