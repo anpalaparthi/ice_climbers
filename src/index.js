1e426481f8d00ab1eb6a652bcfc7aaf255b03b3c
dhruvashok <47910811+dhruvashok@users.noreply.github.com>
2020-05-31 15:46:57 -0700

    fixed some merge issues

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
->>>>>>> 5d7f74a08b9cdfbc0ac47c359bb72ab030211b5b
